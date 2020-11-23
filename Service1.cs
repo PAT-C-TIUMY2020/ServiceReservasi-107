a29dd5fbb3015dad820b84499eda8e7511da70ad
salis2 <56523603+salis2@users.noreply.github.com>
2020-11-24 00:42:24 +0700

    Add project files.

---
+++ Service1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+
+        }
+        string constring = "Data Source = Salis-R;Initial Catalog=WCFReservasi;Persist Security Info=true;User ID=sa;Password=1234";
+        SqlConnection connection;
+        SqlCommand com; // untuk mengkoneksikan database ke visual studio
+
+        public List<DetailLokasi> DetailLokasi()
+        {
+            List<DetailLokasi> LokasiFull = new List<DetailLokasi>(); //proses untuk mendeclare nama list yg telah dibuat dengan nama baru
+            try
+            {
+                string sql = "select ID_lokasi, Nama_lokasi, Deskripsi_full, Kuota from dbo.Lokasi"; //declare query
+                connection = new SqlConnection(constring); //Fungsi konek ke database
+                com = new SqlCommand(sql, connection); //Proses execute query
+                connection.Open(); //membuka koneksi
+                SqlDataReader reader = com.ExecuteReader(); //Menampilkan data query
+                while (reader.Read())
+                {
+                    /*nama Class*/
+                    DetailLokasi data = new DetailLokasi(); //deskripsi data, mengambil 1per1 dari database
+                    //bentuk query
+                    data.IDLokasi = reader.GetString(0); //0 itu index, ada dikolom keberapa di string sql diatas
+                    data.NamaLokasi = reader.GetString(1); //
+                    data.DeskripsiFull = reader.GetString(2);
+                    data.Kuota = reader.GetInt32(3);
+                    LokasiFull.Add(data); //mengumpulkan data yang awalnya dari array
+                }
+                connection.Close(); //untuk menutup akses ke database
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return LokasiFull;
+        }
+
+        public string pemesanan(string IDPemesanan, string NamaCostumer, string NoTelpon, int JumlahPemesanan, string IDLokasi)
+        {
+            string a = "gagal";
+            try
+            {
+                string sql = "insert into dbo.Pemesanan values ('" + IDPemesanan + "', '" + NamaCostumer + "', '" + NoTelpon + "', '" + JumlahPemesanan + "', '" + IDLokasi + "')";
+                connection = new SqlConnection(constring); //konek ke database
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+                a = "sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+            return a;
+        }
+    }
+}
